58522134717f17c392799ec538dc15c88e243d5d
sangtran <sangdonga25@gmail.com>
2021-03-07 23:33:19 +0700

    call services

--- ew-rest-api/src/main/java/com/ewallet/rest/controller/SubscriberController.java
+++ ew-rest-api/src/main/java/com/ewallet/rest/controller/SubscriberController.java
@@ -2,10 +2,14 @@ package com.ewallet.rest.controller;
 
 import com.ewallet.rest.model.Subscriber;
 import com.ewallet.rest.request.SubscriberRegisterRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
@@ -15,6 +19,10 @@ public class SubscriberController {
 
     @PostMapping("/register")
     public SubscriberRegisterRequest registerNewSubscriber(@RequestBody SubscriberRegisterRequest subscriberRegisterPayload) {
+        final String url = "http://localhost:8001/subscriber/save";
+        RestTemplate restTemplate = new RestTemplate();
+        Subscriber res = restTemplate.postForObject(url, subscriberRegisterPayload, Subscriber.class);
+        System.out.println(res);
         return subscriberRegisterPayload;
     }
 
