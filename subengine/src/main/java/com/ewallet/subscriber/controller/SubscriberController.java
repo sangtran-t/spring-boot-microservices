34aaa17ba6fdf7c73a16a77f7bcd272056d9a0e2
sangtran <sangdonga25@gmail.com>
2021-03-08 17:13:57 +0700

    start rabbitmq server

--- subengine/src/main/java/com/ewallet/subscriber/controller/SubscriberController.java
+++ subengine/src/main/java/com/ewallet/subscriber/controller/SubscriberController.java
@@ -2,19 +2,26 @@ package com.ewallet.subscriber.controller;
 
 import com.ewallet.subscriber.model.Subscriber;
 import com.ewallet.subscriber.service.SubscriberService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/subscriber")
 public class SubscriberController {
 
+    @Value("${app.message}")
+    private String message;
+
     @Autowired
     private SubscriberService subscriberService;
 
     @PostMapping("/save")
-    public Subscriber saveSubscriber(@RequestBody Subscriber subscriber) {
-        return subscriberService.saveSubscriber(subscriber);
+    public String saveSubscriber(@RequestBody Subscriber subscriber) {
+//        subscriberService.send(subscriber);
+//        return subscriberService.saveSubscriber(subscriber);
+        return message;
     }
 
     @GetMapping("/info/{id}")
--- subengine/src/main/java/com/ewallet/subscriber/service/SubscriberService.java
+++ subengine/src/main/java/com/ewallet/subscriber/service/SubscriberService.java
@@ -2,11 +2,21 @@ package com.ewallet.subscriber.service;
 
 import com.ewallet.subscriber.model.Subscriber;
 import com.ewallet.subscriber.repository.SubscriberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+
+@Component
 @Service
-public class SubscriberService {
+public class SubscriberService implements RabbitListenerConfigurer {
+
+    private static final Logger logger = LoggerFactory.getLogger(SubscriberService.class);
 
     @Autowired
     private SubscriberRepository subscriberRepository;
@@ -17,5 +27,15 @@ public class SubscriberService {
 
     public Subscriber getSubscriberById(Long id) {
         return subscriberRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
+
+    }
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void receivedMessage(Subscriber subscriber) {
+        logger.info("Subscriber Details Received is.. " + subscriber);
     }
 }
